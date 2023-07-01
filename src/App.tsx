b2eda56eae5863a68388561b3192759c3378cd77
shubham kumar <kumarshubham417@gmail.com>
2023-07-01 20:53:29 +0530

    count state creted with type number

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './Components/Counter';
 
+interface ICounter {
+  count: number;
+}
+
 function App() {
+  const [count, setCount] = useState<number>();
+
   return (
     <div className="App">
-     <Counter />
+      <h1>{count}</h1>
+      <Counter setCount={setCount} />
     </div>
   );
 }
--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Counter() {
+function Counter({setCount}) {
   return (
     <div>Counter</div>
   )
